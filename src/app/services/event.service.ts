b624f3a699527c761fdb55d104596e63726f9c6a
TeamW3villa <techdev@w3villa.com>
2020-10-07 22:09:03 +0530

    common loader

---
+++ src/app/services/event.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  constructor() { }
+
+  private showLoaderEvent = new Subject<boolean>();
+  showLoader$ = this.showLoaderEvent.asObservable();
+
+  showLoader(isLoading: boolean) {
+    this.showLoaderEvent.next(isLoading);
+  }
+}
---
+++ src/app/shared/common-loader/common-loader.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../services/event.service';
+
+@Component({
+  selector: 'app-common-loader',
+  templateUrl: './common-loader.component.html',
+  styleUrls: ['./common-loader.component.scss']
+})
+export class CommonLoaderComponent implements OnInit {
+
+	isLoading = false;
+  constructor(private eventService: EventService) { 
+  	this.eventService.showLoader$.subscribe(isLoading => {
+  		this.isLoading = isLoading;
+  	});
+  }
+
+  ngOnInit(): void {
+
+  	
+  }
+
+}
