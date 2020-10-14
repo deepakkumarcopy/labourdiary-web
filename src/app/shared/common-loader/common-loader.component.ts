fe66104ebb984cdcfed6fe844fd5fd7e3a6bc8d9
w3villa-deepak <deepak.kumar2@w3villa.com>
2020-10-14 23:36:44 +0530

    fix indent

--- src/app/shared/common-loader/common-loader.component.ts
+++ src/app/shared/common-loader/common-loader.component.ts
@@ -2,22 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../services/event.service';
 
 @Component({
-  selector: 'app-common-loader',
-  templateUrl: './common-loader.component.html',
-  styleUrls: ['./common-loader.component.scss']
+	selector: 'app-common-loader',
+	templateUrl: './common-loader.component.html',
+	styleUrls: ['./common-loader.component.scss']
 })
+
 export class CommonLoaderComponent implements OnInit {
 
 	isLoading = false;
-  constructor(private eventService: EventService) { 
-  	this.eventService.showLoader$.subscribe(isLoading => {
-  		this.isLoading = isLoading;
-  	});
-  }
 
-  ngOnInit(): void {
+	constructor(private eventService: EventService) {
+		this.eventService.showLoader$.subscribe(isLoading => {
+			this.isLoading = isLoading;
+		});
+	}
 
-  	
-  }
-
+	ngOnInit(): void {
+	}
 }
