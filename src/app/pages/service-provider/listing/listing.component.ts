3bb7b3cf6567a88e1f365f4fcd8dc5e679e3c036
TeamW3villa <techdev@w3villa.com>
2020-10-23 22:59:16 +0530

    dynamic listing page

--- src/app/pages/service-provider/listing/listing.component.ts
+++ src/app/pages/service-provider/listing/listing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router, RouterEvent, NavigationEnd } from '@angular/router';
+import { ApiService } from '../../../services/api.service';
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingComponent implements OnInit {
 
-  constructor() { }
+  user: any = JSON.parse(localStorage.getItem('user'));
+  services:any = []
+  isLoading:boolean = true;
+  constructor(private api: ApiService,
+    private router: Router,) { }
 
   ngOnInit(): void {
-  }
 
+    this.getUserService();
+  }
+  getUserService() {
+    this.api.getUsersService(this.user.id).subscribe((res) => {
+      if (res.success) {
+        this.isLoading = false;
+        this.services = res.services;
+
+      } else {
+        this.isLoading = false;
+      }
+    }, (e) => {
+        this.isLoading = false;
+        console.log(e);
+    })
+  }
 }
