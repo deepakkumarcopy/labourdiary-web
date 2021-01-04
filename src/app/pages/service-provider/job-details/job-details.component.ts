8c7c0ee6460d23481e9417dbde0ba1119e89fb91
TeamW3villa <techdev@w3villa.com>
2021-01-04 12:40:01 +0530

    api integration to show job order

--- src/app/pages/service-provider/job-details/job-details.component.ts
+++ src/app/pages/service-provider/job-details/job-details.component.ts
@@ -15,7 +15,8 @@ declare var jQuery: any;
   styleUrls: ['./job-details.component.scss']
 })
 export class JobDetailsComponent implements OnInit {
-
+  isLoading:boolean = true
+  order:any
   constructor(private modalService: ModalService,
 		private api: ApiService,
 		private route: ActivatedRoute,
@@ -24,11 +25,51 @@ export class JobDetailsComponent implements OnInit {
 		private customValidation: CustomValidationService) { }
 
   ngOnInit(): void {
+    // this.getTimeSlot()
+    this.route.params.subscribe((params) => {
+      console.log(params,'paramssssssssssssssss')
+			if (!!params && params.jobId) {
+				this.getOrderDetails(params.jobId)
+			}
+		})
   }
   openModal(id) {
     this.modalService.open(id)
   }
   closeModal(id) {
     this.modalService.close(id)
+  }
+  getOrderDetails(id) {
+    this.api.getOrder(id).subscribe((res) => { 
+      console.log(res, 'response of job details')
+      if(res.success) {
+        this.isLoading = false;
+        this.order = res.order[0]
+        if(this.order){
+          this.getTimeSlot(this.order.service.id)
+        }
+      }   else {
+        this.isLoading = false
+      }
+    },err=>{
+        this.isLoading = false;
+        console.log('something went wrong')
+    });
+
+    
+  }
+  getTimeSlot(id) {
+    this.api.getTimeSlots(id).subscribe((res) => {
+      console.log(res, 'responseeeeeeeeeeeeeeeeeeeeeeeeeeee')
+      if (res.success) {
+        // this.isLoading = false;
+       
+      } else {
+        // this.isLoading =false
+      }
+    },err=>{
+        // this.isLoading = false;
+        console.log('something went wrong')
+    });
   }
 }
